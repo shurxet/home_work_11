724def6b400bfd7212d9e647f496d019c239f917
sshur <s.shurupov.88@mail.ru>
2022-05-31 14:39:06 +0300

    First commit

---
+++ utils.py
@@ -0,0 +1,43 @@
+import json
+
+__data = []
+
+def load_candidates_from_json(path):
+    global __data
+    with open(path, 'r', encoding='utf-8') as file:
+        __data = json.load(file)
+    return __data
+
+def get_candidate(candidate_id):
+    for candidate in __data:
+        if candidate['id'] == candidate_id:
+            return {
+                'name': candidate['name'],
+                'position': candidate['position'],
+                'picture': candidate['picture'],
+                'skills': candidate['skills']
+            }
+    return {'not_found': 'Ушёл на обед'}
+
+def get_candidates_by_name(candidate_name):
+    return [candidate for candidate in __data if candidate_name.lower() in candidate['skills'].lower()]
+
+def get_candidates_by_skill(skill_name):
+    candidaes = []
+    for candidate in __data:
+        skills = candidate['skills'].lower().split(', ')
+        if skill_name.lower() in skills:
+            candidaes.append(candidate)
+    return candidaes
+
+
+
+
+
+
+
+
+
+
+
+
